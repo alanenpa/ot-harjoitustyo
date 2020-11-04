263b351527033e6f7d5c6b9380fc03981cd3ca6c
Patrik Alanen <72764484+alanenpa@users.noreply.github.com>
2020-11-04 12:59:55 +0200

    Päivitä luokkaa MaksukorttiTest

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -7,14 +7,63 @@ import org.junit.Test;
 public class MaksukorttiTest {
 
     Maksukortti kortti;
+    Kassapaate kassapaate;
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
+        kassapaate = new Kassapaate();
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        Maksukortti kortti2 = new Maksukortti(10000);
+        assertEquals(10000,kortti2.saldo());
+    }
+
+    @Test
+    public void kortiltaEiVoiOttaaLiikaaRahaa() {
+        assertFalse(kortti.otaRahaa(1500));
+    }
+
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(2500);
+        assertEquals("saldo: 35.0", kortti.toString());
+    }
+
+    @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+        kassapaate.syoEdullisesti(kortti);
+        assertEquals("saldo: 7.60", kortti.toString());
+    }
+
+    @Test
+    public void syoMaukkaastiVahentaaSaldoaOikein() {
+        kassapaate.syoMaukkaasti(kortti);
+        assertEquals("saldo: 6.0", kortti.toString());
+    }
+
+    @Test
+    public void saldoEiMuutuJosLiianVähänRahaa() {
+        Maksukortti kortti2 = new Maksukortti(200);
+        kassapaate.syoEdullisesti(kortti2);
+        assertEquals("saldo: 2.0", kortti2.toString());
+    }
+
+    @Test
+    public void metodiPalauttaaTrueJosRahaaTarpeeksi() {
+        assertTrue(kassapaate.syoMaukkaasti(kortti));
+    }
+
+    @Test
+    public void metodiPalauttaaFalseJosRahaaEiTarpeeksi() {
+        Maksukortti kortti2 = new Maksukortti(200);
+        assertFalse(kassapaate.syoMaukkaasti(kortti2));
     }
 }
